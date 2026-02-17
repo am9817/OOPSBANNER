547ecc69a68108d3ae3878c8be0a2040a8c11c9f
Aritra-Mukhopadhyay <aritramukhopadhyay1010@gmail.com>
2026-02-17 11:17:57 +0530

    Print OOPS to console

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,6 +1,6 @@
 public class oopsbannerapp
 {
     public static void main (String[] args){
-        System.out.println("Welcome to OOPS Banner repo");
+        System.out.println("OOPS");
     }
 }
