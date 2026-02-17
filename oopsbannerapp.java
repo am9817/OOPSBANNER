c2453bc57be967c770f1361a5d7722704f52202b
Aritra-Mukhopadhyay <aritramukhopadhyay1010@gmail.com>
2026-02-17 11:23:36 +0530

    Print OOPS Banner using Asterisks

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,6 +1,13 @@
 public class oopsbannerapp
 {
-    public static void main (String[] args){
-        System.out.println("OOPS");
+   public static void main(String[] args) {
+        // Line-by-line rendering of the "OOPS" Banner
+        System.out.println("  ***   ***  ****  ***** ");
+        System.out.println(" *   * *   * *   * *     ");
+        System.out.println(" *   * *   * *   * *     ");
+        System.out.println(" *   * *   * ****  ***** ");
+        System.out.println(" *   * *   * *         * ");
+        System.out.println(" *   * *   * *         * ");
+        System.out.println("  ***   ***  *     ***** ");
     }
 }
