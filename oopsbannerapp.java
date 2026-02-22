9f18739930c55b32f91fce9a8267ca46436779cf
Aritra-Mukhopadhyay <aritramukhopadhyay1010@gmail.com>
2026-02-23 02:27:40 +0530

    UPGRADED TO UC4 standards

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,13 +1,18 @@
 public class oopsbannerapp
 {
    public static void main(String[] args) {
-        // Line-by-line rendering of the "OOPS" Banner
-        System.out.println("  ***   ***  ****  ***** ");
-        System.out.println(" *   * *   * *   * *     ");
-        System.out.println(" *   * *   * *   * *     ");
-        System.out.println(" *   * *   * ****  ***** ");
-        System.out.println(" *   * *   * *         * ");
-        System.out.println(" *   * *   * *         * ");
-        System.out.println("  ***   ***  *     ***** ");
+        String[] banner = {
+            " *****     *****     *****      ***** ",
+            "*     *   *     *   *     *    *     *",
+            "*     *   *     *   *     *    *      ",
+            "*     *   *     *   *****      *****  ",
+            "*     *   *     *   *              *  ",
+            "*     *   *     *   *        *     *  ",
+            " *****     *****    *         *****   "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }gi
     }
 }
